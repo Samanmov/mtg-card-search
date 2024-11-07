f8a9160665267103c4d8c60d3dbf908d39f08abd
Samanmov <saman_mov@yahoo.com>
2024-11-07 10:00:42 +0100

    Explore and integrate image generation feature

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { thunk } from "redux-thunk";
 import { cardsReducers } from "./features/cardSearch/reducers/cardsReducers";
+import { randomImageReducers } from "./features/randomImage/reducers/randomImageReducers";
 
 const rootReducer = combineReducers({
   cards: cardsReducers,
+  randomImage: randomImageReducers,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
